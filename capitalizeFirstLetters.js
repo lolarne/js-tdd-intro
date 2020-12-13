27d4e1efed425b85b62aadf383ce6e25636d3d26
lolarne <rne.lola@gmail.com>
2020-12-13 16:08:32 +0100

    added assertions

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+// Check that capitalizeFirst is a function
+
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+
+// Check that capitalizeFirst accepts one argument
+
+assert.strictEqual(capitalizeFirstLetters.length, 1);
+
+// Check that capitalizeFirst transforms javaScript correctly
+
+assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
+
+// Check that it works for a 1-character string
+
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+
+// Check that it works for an empty string
+
+assert.strictEqual(capitalizeFirstLetters(''), '');
+
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
